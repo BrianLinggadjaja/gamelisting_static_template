cba3a051a53089278c029267506a6961e853033d
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-09-29 15:18:41 -0700

    Create homepage

---
+++ js/activeRoute.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', ()=> {
+    const routeName = window.location.pathname.toString().toLowerCase()
+    const route = document.querySelector('[data-route="' + routeName + '"]')
+
+    route.setAttribute('active', '')
+})
