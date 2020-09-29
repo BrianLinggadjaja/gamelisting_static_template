0a372f38b0bf52b6f4c34261b445109c3077d473
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-09-29 15:31:30 -0700

    Update root dir data attr

--- js/activeRoute.js
+++ js/activeRoute.js
@@ -1,6 +1,9 @@
+// Should be placed in a .env; this is just used for sake of ease
+const rootDir = 'gamelisting_static_template'
+
 window.addEventListener('load', ()=> {
     const routeName = window.location.pathname.toString().toLowerCase()
-    const route = document.querySelector('[data-route="' + routeName + '"]')
+    const route = document.querySelector('[data-route="' + rootDir + '/' + routeName + '"]')
 
     route.setAttribute('active', '')
 })
