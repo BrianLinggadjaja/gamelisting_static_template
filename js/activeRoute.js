1b29c1d1cd376540b173ebd734019cba4a6fad89
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-09-29 17:11:47 -0700

    Fixed active route finder

--- js/activeRoute.js
+++ js/activeRoute.js
@@ -1,9 +1,6 @@
-// Should be placed in a .env; this is just used for sake of ease
-const rootDir = '/gamelisting_static_template'
-
 window.addEventListener('load', () => {
     const routeName = window.location.pathname.toString().toLowerCase()
-    const route = document.querySelector('[data-route="' + rootDir + routeName + '"]')
+    const route = document.querySelector('[data-route="' + routeName + '"]')
 
     route.setAttribute('active', '')
 })
